f20c41ecf1c03e7750980f99a00f92ff2be5ed33
OldBigBuddha <development@oldbigbuddha.net>
2020-11-10 02:12:57 +0900

    [update] get_addtional_field
    This commit make possible to cast value with function paramater from str.

--- TwitterAPIv2/util.py
+++ TwitterAPIv2/util.py
@@ -1,8 +1,11 @@
 from typing import Any, Optional
 
 
-def get_additional_field(data: dict, key: str) -> Optional[Any]:
+def get_additional_field(data: dict, key: str, converter=None) -> Optional[Any]:
     if key in data.keys():
-        return data[key]
+        if converter:
+            return converter(data[key])
+        else:
+            return data[key]
     else:
         return None
