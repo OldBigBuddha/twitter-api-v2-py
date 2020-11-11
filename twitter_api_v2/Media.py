8bbf01b61568ae05e682fdcf45e5cf1fa05bef51
OldBigBuddha <development@oldbigbuddha.net>
2020-11-11 11:40:05 +0900

    [fix] apply black formatting

--- twitter_api_v2/Media.py
+++ twitter_api_v2/Media.py
@@ -6,9 +6,9 @@ from twitter_api_v2.util import get_additional_field
 
 class Type(Enum):
 
-    GIF: str = 'GIF'
-    PHOTO: str = 'photo'
-    VIDEO: str = 'video'
+    GIF: str = "GIF"
+    PHOTO: str = "photo"
+    VIDEO: str = "video"
 
     def __str__(self) -> str:
         return self.value
@@ -16,31 +16,34 @@ class Type(Enum):
 
 class Field(Enum):
 
-    HEIGHT: str = 'height'
-    WIDTH: str = 'width'
-    VIEW_COUNT: str = 'public_metrics'
-    DURATION_MS: str = 'duration_ms'
-    PREVIEW_IMAGE_URL: str = 'preview_image_url'
+    HEIGHT: str = "height"
+    WIDTH: str = "width"
+    VIEW_COUNT: str = "public_metrics"
+    DURATION_MS: str = "duration_ms"
+    PREVIEW_IMAGE_URL: str = "preview_image_url"
 
     def __str__(self) -> str:
         return self.value
 
 
 class Media:
-
     def __init__(self, data: dict) -> None:
 
         # default
-        self.media_key: str = data['media_key']
-        self.type: Type = Type(data['type'])
+        self.media_key: str = data["media_key"]
+        self.type: Type = Type(data["type"])
 
         # additional
         self.height: Optional[int] = get_additional_field(data, Field.HEIGHT.value, int)
         self.width: Optional[int] = get_additional_field(data, Field.WIDTH.value, int)
-        self.duration_ms: Optional[int] = get_additional_field(data, Field.DURATION_MS.value, int)
-        self.preview_image_url: Optional[str] = get_additional_field(data, Field.PREVIEW_IMAGE_URL.value)
+        self.duration_ms: Optional[int] = get_additional_field(
+            data, Field.DURATION_MS.value, int
+        )
+        self.preview_image_url: Optional[str] = get_additional_field(
+            data, Field.PREVIEW_IMAGE_URL.value
+        )
 
         self.view_count: Optional[int] = None
         public_metric = get_additional_field(data, Field.VIEW_COUNT.value)
         if public_metric:
-            self.view_count = int(public_metric['view_count'])
+            self.view_count = int(public_metric["view_count"])
--- twitter_api_v2/TwitterAPI.py
+++ twitter_api_v2/TwitterAPI.py
@@ -18,47 +18,58 @@ class TwitterAPI:
     def __init__(self, bearer_token: str) -> None:
         self.__BEARER_TOKEN: str = bearer_token
         self.__REQUEST_HEADERS: Dict = {
-            'Authorization': f'Bearer {self.__BEARER_TOKEN}'
+            "Authorization": f"Bearer {self.__BEARER_TOKEN}"
         }
 
-        self.__API_URL: str = 'https://api.twitter.com/2/tweets'
+        self.__API_URL: str = "https://api.twitter.com/2/tweets"
 
     def get_tweet(
-            self,
-            id: str,
-            expansions: List[Tweet.Expantion] = [],
-            tweet_fields: List[Tweet.Field] = [],
-            media_fields: List[Media.Field] = [],
-            poll_fields: List[Poll.Field] = []) -> Tweet.Tweet:
+        self,
+        id: str,
+        expansions: List[Tweet.Expantion] = [],
+        tweet_fields: List[Tweet.Field] = [],
+        media_fields: List[Media.Field] = [],
+        poll_fields: List[Poll.Field] = [],
+    ) -> Tweet.Tweet:
 
-        params: Optional[Dict[str, str]] = self._make_params(expansions, tweet_fields, media_fields, poll_fields)
+        params: Optional[Dict[str, str]] = self._make_params(
+            expansions, tweet_fields, media_fields, poll_fields
+        )
         logger.debug(params)
-        response = requests.get(url=f'{self.__API_URL}/{id}', params=params, headers=self.__REQUEST_HEADERS)
+        response = requests.get(
+            url=f"{self.__API_URL}/{id}", params=params, headers=self.__REQUEST_HEADERS
+        )
         res_json = json.loads(response.text)
         logger.debug(res_json)
-        if 'includes' in res_json.keys():
-            return Tweet.Tweet(**res_json['data'], **res_json['includes'])
+        if "includes" in res_json.keys():
+            return Tweet.Tweet(**res_json["data"], **res_json["includes"])
         else:
-            return Tweet.Tweet(**res_json['data'])
+            return Tweet.Tweet(**res_json["data"])
 
     def _make_params(
-            self,
-            expansions: List[Tweet.Expantion],
-            tweet_fields: List[Tweet.Field],
-            media_fields: List[Media.Field],
-            poll_fields: List[Poll.Field]) -> Optional[Dict[str, str]]:
+        self,
+        expansions: List[Tweet.Expantion],
+        tweet_fields: List[Tweet.Field],
+        media_fields: List[Media.Field],
+        poll_fields: List[Poll.Field],
+    ) -> Optional[Dict[str, str]]:
 
-        if (not expansions) and (not tweet_fields) and (not media_fields) and (not poll_fields):
+        if (
+            (not expansions)
+            and (not tweet_fields)
+            and (not media_fields)
+            and (not poll_fields)
+        ):
             return None
 
         params: Dict[str, str] = {}
         if expansions:
-            params['expansions'] = ','.join(list(map(str, expansions)))
+            params["expansions"] = ",".join(list(map(str, expansions)))
         if tweet_fields:
-            params['tweet.fields'] = ','.join(list(map(str, tweet_fields)))
+            params["tweet.fields"] = ",".join(list(map(str, tweet_fields)))
         if media_fields:
-            params['media.fields'] = ','.join(list(map(str, media_fields)))
+            params["media.fields"] = ",".join(list(map(str, media_fields)))
         if poll_fields:
-            params['poll.fields'] = ','.join(list(map(str, poll_fields)))
+            params["poll.fields"] = ",".join(list(map(str, poll_fields)))
 
         return params
