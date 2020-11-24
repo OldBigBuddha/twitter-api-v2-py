feb7d4e078bb21c0a5ff105a3e6ab5c98663f70e
OldBigBuddha <development@oldbigbuddha.net>
2020-11-24 09:43:23 +0900

    [add] entity for profile description and url
    When User.Field.ENTITIES is specified, user.description will have more detail information.
    For example hashtag in user description.

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,6 +5,7 @@ from typing import Dict, List
 import pytest
 
 from twitter_api_v2 import TwitterAPI, User
+from twitter_api_v2.Entities import Url
 
 
 @pytest.fixture
@@ -14,6 +15,7 @@ def client() -> TwitterAPI.TwitterAPI:
 
 def test_get_user_by_id(client: TwitterAPI.TwitterAPI) -> None:
 
+    # ref: https://twitter.com/TwitterDev
     SAMPLE_USER: Dict = {
         "id": "2244994945",
         "name": "Twitter Dev",
@@ -29,6 +31,7 @@ def test_get_user_by_id(client: TwitterAPI.TwitterAPI) -> None:
 
 def test_get_user_by_username(client: TwitterAPI.TwitterAPI) -> None:
 
+    # ref: https://twitter.com/TwitterDev
     SAMPLE_USER: Dict = {
         "id": "2244994945",
         "name": "Twitter Dev",
@@ -42,14 +45,21 @@ def test_get_user_by_username(client: TwitterAPI.TwitterAPI) -> None:
     assert user.username == SAMPLE_USER["username"], "username field is wrong."
 
 
-def test_not_exist_user(client: TwitterAPI.TwitterAPI) -> None:
+def test_not_exist_user_by_id(client: TwitterAPI.TwitterAPI) -> None:
 
     with pytest.raises(Exception):
         client.get_user_by_id("")
 
 
+def test_not_exist_user_by_username(client: TwitterAPI.TwitterAPI) -> None:
+
+    with pytest.raises(Exception):
+        client.get_user_by_username("")
+
+
 def test_user_with_additional_fields(client: TwitterAPI.TwitterAPI) -> None:
 
+    # ref: https://twitter.com/TwitterDev
     SAMPLE_USER: Dict = {
         # default fields
         "id": "2244994945",
@@ -57,7 +67,9 @@ def test_user_with_additional_fields(client: TwitterAPI.TwitterAPI) -> None:
         "username": "TwitterDev",
         # additional fields
         "created_at": "2013-12-14T04:35:55.000+00:00",
-        "description": "The voice of the #TwitterDev team and your official source for updates, news, and events, related to the #TwitterAPI.",
+        "description": {
+            "text": "The voice of the #TwitterDev team and your official source for updates, news, and events, related to the #TwitterAPI.",
+        },
         "location": "127.0.0.1",
         "url": "https://t.co/3ZX3TNiZCY",
         "profile_image_url": "https://pbs.twimg.com/profile_images/1283786620521652229/lEODkLTh_normal.jpg",
@@ -87,7 +99,10 @@ def test_user_with_additional_fields(client: TwitterAPI.TwitterAPI) -> None:
     assert user.created_at == datetime.fromisoformat(
         SAMPLE_USER["created_at"]
     ), "created_at is wrong."
-    assert user.description == SAMPLE_USER["description"], "description field is wrong."
+    assert user.description, "description should exit."
+    assert (
+        user.description.text == SAMPLE_USER["description"]["text"]
+    ), "description field is wrong."
     assert user.location == SAMPLE_USER["location"], "location field is wrong."
     assert (
         user.pinned_tweet_id == SAMPLE_USER["pinned_tweet_id"]
@@ -102,6 +117,7 @@ def test_user_with_additional_fields(client: TwitterAPI.TwitterAPI) -> None:
 
 def test_public_metric_for_user(client: TwitterAPI.TwitterAPI) -> None:
 
+    # ref: https://twitter.com/TwitterDev
     user: User.User = client.get_user_by_id(
         "2244994945", user_fields=[User.Field.PUBLIC_METRICS]
     )
@@ -111,3 +127,67 @@ def test_public_metric_for_user(client: TwitterAPI.TwitterAPI) -> None:
     assert user.public_metrics.following_count, "following_count should exist."
     assert user.public_metrics.tweet_count, "tweet_count should exist."
     assert user.public_metrics.listed_count, "listed_count should exist."
+
+
+def test_entities_for_user(client: TwitterAPI.TwitterAPI) -> None:
+
+    # ref: https://twitter.com/TwitterDev
+    SAMPLE_USER: Dict = {
+        # default fields
+        "id": "2244994945",
+        "name": "Twitter Dev",
+        "username": "TwitterDev",
+        # additional fields
+        "description": {
+            "text": "The voice of the #TwitterDev team and your official source for updates, news, and events, related to the #TwitterAPI.",
+            "hashtags": [
+                {"start": 17, "end": 28, "tag": "TwitterDev"},
+                {"start": 105, "end": 116, "tag": "TwitterAPI"},
+            ],
+        },
+        "url": {
+            "start": 0,
+            "end": 23,
+            "url": "https://t.co/3ZX3TNiZCY",
+            "expanded_url": "https://developer.twitter.com/en/community",
+            "display_url": "developer.twitter.com/en/community",
+        },
+    }
+
+    user: User.User = client.get_user_by_id(
+        SAMPLE_USER["id"], [User.Field.ENTITIES, User.Field.DESCRIPTION]
+    )
+
+    assert user.id == SAMPLE_USER["id"], "User ID is wrong."
+    assert user.name == SAMPLE_USER["name"], "name field is wrong."
+    assert user.username == SAMPLE_USER["username"], "username field is wrong."
+
+    # description
+    assert user.description, "User description should exist."
+    assert (
+        user.description.text == SAMPLE_USER["description"]["text"]
+    ), "description's text is wrong."
+    assert user.description.hashtags, "hashtags should exist."
+    for idx, hashtag in enumerate(user.description.hashtags):
+        assert (
+            hashtag.start == SAMPLE_USER["description"]["hashtags"][idx]["start"]
+        ), f"hashtags[{idx}]: start is wrong."
+        assert (
+            hashtag.end == SAMPLE_USER["description"]["hashtags"][idx]["end"]
+        ), f"hashtags[{idx}]: end is wrong."
+        assert (
+            hashtag.tag == SAMPLE_USER["description"]["hashtags"][idx]["tag"]
+        ), f"hashtags[{idx}]: tag is wrong."
+
+    # url
+    assert isinstance(user.url, Url), "url's type is wrong"
+
+    assert user.url.url == SAMPLE_USER["url"]["url"], "url is wrong."
+    assert user.url.start == SAMPLE_USER["url"]["start"], "url start is wrong."
+    assert user.url.end == SAMPLE_USER["url"]["end"], "url end is wrong."
+    assert (
+        user.url.expanded_url == SAMPLE_USER["url"]["expanded_url"]
+    ), "expanded_url is wrong."
+    assert (
+        user.url.display_url == SAMPLE_USER["url"]["display_url"]
+    ), "display_url is wrong."
--- twitter_api_v2/User.py
+++ twitter_api_v2/User.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from enum import Enum
-from typing import Dict, Optional
+from typing import Dict, List, Optional, Union
 
+from twitter_api_v2.Entities import CashTag, HashTag, Mention, Url
 from twitter_api_v2.Metric import Metric
 from twitter_api_v2.util import get_additional_field
 
@@ -24,6 +25,20 @@ class Field(Enum):
         return self.value
 
 
+class Description:
+    def __init__(
+        self,
+        text: str = "",
+    ) -> None:
+
+        self.text: str = text
+
+        self.cashtags: Optional[List[CashTag]] = None
+        self.hashtags: Optional[List[HashTag]] = None
+        self.mentions: Optional[List[Mention]] = None
+        self.urls: Optional[List[Url]] = None
+
+
 class PublicMetric(Metric):
     def __init__(self, data: Dict[str, int]) -> None:
         self.followers_count: int = data["followers_count"]
@@ -42,10 +57,56 @@ class User:
         self.created_at: Optional[datetime] = None
         if (created_at := get_additional_field(kwargs, "created_at")) is not None:
             self.created_at = datetime.fromisoformat(created_at.replace("Z", "+00:00"))
-        self.description: Optional[str] = get_additional_field(kwargs, "description")
 
-        # TODO: Implement Entity object
-        self.entities: Optional[Dict] = None
+        self.description: Optional[Description] = None
+        if (text := get_additional_field(kwargs, "description")) :
+            description: Description = Description(text)
+
+            if (
+                "entities" in kwargs.keys()
+                and "description" in kwargs["entities"].keys()
+            ):
+                description_res: Dict = kwargs["entities"]["description"]
+
+                if "cashtags" in description_res.keys():
+                    cashtags: List[Dict] = description_res["cashtags"]
+                    description.cashtags = []
+                    for cashtag in cashtags:
+                        description.cashtags.append(
+                            CashTag(cashtag["start"], cashtag["end"], cashtag["tag"])
+                        )
+
+                if "hashtags" in description_res.keys():
+                    hashtags: List[Dict] = description_res["hashtags"]
+                    description.hashtags = []
+                    for hashtag in hashtags:
+                        description.hashtags.append(
+                            HashTag(hashtag["start"], hashtag["end"], hashtag["tag"])
+                        )
+
+                if "mentions" in description_res.keys():
+                    mentions: List[Dict] = description_res["mentions"]
+                    description.mentions = []
+                    for mention in mentions:
+                        description.mentions.append(
+                            Mention(mention["start"], mention["end"], mention["tag"])
+                        )
+
+                if "urls" in description_res.keys():
+                    urls: List[Dict] = description_res["urls"]
+                    description.urls = []
+                    for url in urls:
+                        description.urls.append(
+                            Url(
+                                url["start"],
+                                url["end"],
+                                url["url"],
+                                url["expanded_url"],
+                                url["display_url"],
+                            )
+                        )
+
+            self.description = description
 
         self.location: Optional[str] = get_additional_field(kwargs, "location")
         self.pinned_tweet_id: Optional[str] = get_additional_field(
@@ -58,8 +119,19 @@ class User:
         self.public_metrics: Optional[PublicMetric] = get_additional_field(
             kwargs, "public_metrics", PublicMetric
         )
-        self.url: Optional[str] = get_additional_field(kwargs, "url")
-        self.verified: Optional[bool] = get_additional_field(kwargs, "verified")
+
+        self.url: Optional[Union[str, Url]] = get_additional_field(kwargs, "url")
+        if "entities" in kwargs.keys() and "url" in kwargs["entities"].keys():
+            url_res: Dict = kwargs["entities"]["url"]["urls"][0]
+            self.url = Url(
+                url_res["start"],
+                url_res["end"],
+                url_res["url"],
+                url_res["expanded_url"],
+                url_res["display_url"],
+            )
+
+        self.verified: Optional[bool] = get_additional_field(kwargs, "verified", bool)
 
         # TODO: Check https://help.twitter.com/en/rules-and-policies/tweet-withheld-by-country
         self.withheld: Optional[Dict] = None
